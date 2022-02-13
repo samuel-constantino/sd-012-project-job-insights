8769798a99d03f3741909b3ebf7a0b0b21b4f4cc
Samuel Constantino <samuelicapuidados@gmail.com>
2022-02-13 12:04:02 -0300

    requisito 5

--- src/insights.py
+++ src/insights.py
@@ -66,21 +66,15 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs = read(path)
 
-    Must call `read`
+    min_salary = min([
+        int(job["min_salary"])
+        for job in jobs
+        if job["min_salary"].isdigit()
+    ])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min_salary
 
 
 def matches_salary_range(job, salary):
